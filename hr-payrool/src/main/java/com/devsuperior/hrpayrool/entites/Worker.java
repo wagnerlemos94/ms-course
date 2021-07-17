1ef3066b1aaea9680668a44d86162a23978923a6
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-07-16 22:53:06 -0300

    RestTamplate

---
+++ hr-payrool/src/main/java/com/devsuperior/hrpayrool/entites/Worker.java
@@ -0,0 +1,19 @@
+package com.devsuperior.hrpayrool.entites;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+}
